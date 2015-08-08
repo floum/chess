a4f86e0f69d5d1c118a296c4f38281521972bf73
Efflam Castel <efflamm.castel@gmail.com>
2015-08-09 01:18:26 +0200

    refactored between?

--- lib/chess/square.rb
+++ lib/chess/square.rb
@@ -14,15 +14,10 @@ module Chess
     end
 
     def between?(square, other_square)
-      return false if [square, other_square].include?(self)
-      if shares_column?(square, other_square)
-        return [self, square, other_square].sort_by(&:row)[1] == self
-      end
-      if shares_row?(square, other_square)
-        return [self, square, other_square].sort_by(&:column)[1] == self
-      end
-      shares_diagonal?(square, other_square) &&
-        [self, square, other_square].sort_by(&:column)[1] == self
+      ![square, other_square].include?(self) &&
+        (vertically_between?(square, other_square) ||
+          horizontally_between?(square, other_square) ||
+          diagonally_between?(square, other_square))
     end
 
     def piece_color
@@ -71,6 +66,21 @@ module Chess
       ([self] + others).combination(2).all? do |s1, s2|
         s1.vertical_distance(s2).abs == s1.horizontal_distance(s2).abs
       end
+    end
+
+    def horizontally_between?(square, other_square)
+      shares_row?(square, other_square) &&
+        [self, square, other_square].sort_by(&:column)[1] == self
+    end
+
+    def vertically_between?(square, other_square)
+      shares_column?(square, other_square) &&
+        [self, square, other_square].sort_by(&:row)[1] == self
+    end
+
+    def diagonally_between?(square, other_square)
+      shares_diagonal?(square, other_square) &&
+        [self, square, other_square].sort_by(&:row)[1] == self
     end
 
     def to_s
